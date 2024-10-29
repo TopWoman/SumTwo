4338fd427502ef33b10d80fc36ae29ed095c0d0e
TopWoman <top.woman.in.the.mai@gmail.com>
2024-10-29 13:12:57 +0300

    первый коммит

---
+++ src/main/java/leetcode/sumtwo/SumtwoApplication.java
@@ -0,0 +1,10 @@
+package leetcode.sumtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumtwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumtwoApplication.class, args
---
+++ src/test/java/leetcode/sumtwo/SumtwoApplicationTests.java
@@ -0,0 +1,10 @@
+package leetcode.sumtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumtwoApplicationTests {
+
+    @Test
+    void contextLoads() {
